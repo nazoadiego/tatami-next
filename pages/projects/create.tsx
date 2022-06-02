acc6875dd7ce7b28250d1195b7646d563d85f92a
diegomarrufo <dmarrufo@ucm.es>
2022-06-02 17:02:41 +0200

    fixed the post form and api call for creating a new project

--- pages/projects/create.tsx
+++ pages/projects/create.tsx
@@ -1,47 +1,31 @@
 import { useRouter } from 'next/router'
 import { FC, FormEvent, useEffect, useState } from 'react'
 import Button from '../../components/Button'
-import type { Project } from '../../types/types'
 
 interface TaskCreateProps {}
 
 const TaskCreate: FC<TaskCreateProps> = () => {
   const router = useRouter()
   const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [projectId, setProjectId] = useState('')
-  const [projects, setProjects] = useState<Project[]>([])
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
 
-    const newTask = { title, description, project_id: projectId }
-    console.log(JSON.stringify(newTask))
-
-    fetch('http://localhost:3000/tasks', {
+    fetch('http://localhost:3000/projects', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(newTask),
+      body: JSON.stringify({ title }),
     }).then(() => {
-      console.log('new blog added')
+      console.log('New project added')
     })
     router.back()
   }
 
-  useEffect(() => {
-    fetch('http://localhost:3000/projects')
-      .then((response) => response.json())
-      .then((data) => {
-        setProjects(data)
-        setProjectId(data[0].id)
-      })
-  }, [])
-
   return (
     <form onSubmit={handleSubmit} className="flex items-center justify-center">
       <div className="flex flex-col space-y-4">
-        <h2 className="text-4xl">Task Create</h2>
-        <label>Task title:</label>
+        <h2 className="text-4xl">Project Create</h2>
+        <label>Project title:</label>
         <input
           type="text"
           required
@@ -49,26 +33,7 @@ const TaskCreate: FC<TaskCreateProps> = () => {
           onChange={(e) => setTitle(e.target.value)}
           className="text-black"
         />
-        <label>Task Description:</label>
-        <textarea
-          required
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="text-black"
-        ></textarea>
-        <label>Task Project:</label>
-        <select
-          value={projectId}
-          onChange={(e) => setProjectId(e.target.value)}
-          className="text-black"
-        >
-          {projects.map((project) => (
-            <option key={project.id} value={project.id}>
-              {project.title}
-            </option>
-          ))}
-        </select>
-        <Button>Add Task</Button>
+        <Button>Add Project</Button>
       </div>
     </form>
   )
